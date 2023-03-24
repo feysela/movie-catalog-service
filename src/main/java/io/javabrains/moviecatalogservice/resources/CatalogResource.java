43515e2061934cc979f5ffbb4a0a22222ea0b5d5
feysela <feysela@gmail.com>
2023-03-24 08:12:35 -0500

    Initial commit

---
+++ src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -0,0 +1,65 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
+    @Autowired
+    MovieInfo movieInfo;
+
+    @Autowired
+    UserRatingInfo userRatingInfo;
+
+    ArrayList<CatalogItem> items = new ArrayList<>();
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        UserRating userRating = userRatingInfo.getUserRating(userId);
+
+        return userRating.getRatings().stream()
+                .map(rating -> movieInfo.getCatalogItem(rating))
+                .collect(Collectors.toList());
+
+       // items.add(new CatalogItem("Tears of the Sun","About a lovely kid", 2));
+        //items.add(new CatalogItem("the man in the iron mask","A leo movie", 10));
+       // return items;
+    }
+
+
+
+
+
+
+
+}
+
+/*
+Alternative WebClient way
+Movie movie = webClientBuilder.build().get().uri("http://localhost:8082/movies/"+ rating.getMovieId())
+.retrieve().bodyToMono(Movie.class).block();
+*/
